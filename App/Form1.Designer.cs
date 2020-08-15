ad2b13b03cd9142a327f138534e1a17a0d399ff4
Anonymous Shrimp <yumiswep@gmail.com>
2020-08-15 14:32:15 -0700

    Serial Connection

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -33,9 +33,23 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.BottomPanel = new System.Windows.Forms.Panel();
+            this.serialButton3 = new System.Windows.Forms.Button();
+            this.serialButton2 = new System.Windows.Forms.Button();
+            this.serialButton1 = new System.Windows.Forms.Button();
+            this.serialText3 = new System.Windows.Forms.TextBox();
+            this.serialText2 = new System.Windows.Forms.TextBox();
+            this.serialText1 = new System.Windows.Forms.TextBox();
+            this.errorText = new System.Windows.Forms.Label();
+            this.serial3 = new System.Windows.Forms.ComboBox();
+            this.serial2 = new System.Windows.Forms.ComboBox();
+            this.serial1 = new System.Windows.Forms.ComboBox();
+            this.serialLabel1 = new System.Windows.Forms.Label();
+            this.serialLabel2 = new System.Windows.Forms.Label();
+            this.serialLabel3 = new System.Windows.Forms.Label();
             this.Toolbar.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.BottomPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Toolbar
@@ -70,28 +84,175 @@
             // 
             // BottomPanel
             // 
+            this.BottomPanel.Controls.Add(this.serialButton3);
+            this.BottomPanel.Controls.Add(this.serialLabel3);
+            this.BottomPanel.Controls.Add(this.serialLabel1);
+            this.BottomPanel.Controls.Add(this.serialLabel2);
+            this.BottomPanel.Controls.Add(this.serialButton2);
+            this.BottomPanel.Controls.Add(this.serialButton1);
+            this.BottomPanel.Controls.Add(this.serialText3);
+            this.BottomPanel.Controls.Add(this.serialText2);
+            this.BottomPanel.Controls.Add(this.serialText1);
+            this.BottomPanel.Controls.Add(this.errorText);
+            this.BottomPanel.Controls.Add(this.serial3);
+            this.BottomPanel.Controls.Add(this.serial2);
+            this.BottomPanel.Controls.Add(this.serial1);
             this.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.BottomPanel.Location = new System.Drawing.Point(155, 252);
+            this.BottomPanel.Location = new System.Drawing.Point(155, 302);
             this.BottomPanel.Name = "BottomPanel";
-            this.BottomPanel.Size = new System.Drawing.Size(645, 198);
+            this.BottomPanel.Size = new System.Drawing.Size(645, 148);
             this.BottomPanel.TabIndex = 1;
+            // 
+            // serialButton3
+            // 
+            this.serialButton3.BackColor = System.Drawing.Color.Transparent;
+            this.serialButton3.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialButton3.Location = new System.Drawing.Point(261, 84);
+            this.serialButton3.Name = "serialButton3";
+            this.serialButton3.Size = new System.Drawing.Size(106, 29);
+            this.serialButton3.TabIndex = 10;
+            this.serialButton3.Text = "button3";
+            this.serialButton3.UseVisualStyleBackColor = false;
+            this.serialButton3.Click += new System.EventHandler(this.serialButton3_Click);
+            // 
+            // serialButton2
+            // 
+            this.serialButton2.BackColor = System.Drawing.Color.Transparent;
+            this.serialButton2.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialButton2.Location = new System.Drawing.Point(261, 46);
+            this.serialButton2.Name = "serialButton2";
+            this.serialButton2.Size = new System.Drawing.Size(106, 29);
+            this.serialButton2.TabIndex = 9;
+            this.serialButton2.Text = "button2";
+            this.serialButton2.UseVisualStyleBackColor = false;
+            this.serialButton2.Click += new System.EventHandler(this.serialButton2_Click);
+            // 
+            // serialButton1
+            // 
+            this.serialButton1.BackColor = System.Drawing.Color.Transparent;
+            this.serialButton1.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serialButton1.Location = new System.Drawing.Point(261, 8);
+            this.serialButton1.Name = "serialButton1";
+            this.serialButton1.Size = new System.Drawing.Size(106, 29);
+            this.serialButton1.TabIndex = 8;
+            this.serialButton1.Text = "button1";
+            this.serialButton1.UseVisualStyleBackColor = false;
+            this.serialButton1.Click += new System.EventHandler(this.serialButton1_Click);
+            // 
+            // serialText3
+            // 
+            this.serialText3.Location = new System.Drawing.Point(19, 87);
+            this.serialText3.Name = "serialText3";
+            this.serialText3.Size = new System.Drawing.Size(100, 29);
+            this.serialText3.TabIndex = 7;
+            this.serialText3.TextChanged += new System.EventHandler(this.serialText3_TextChanged);
+            // 
+            // serialText2
+            // 
+            this.serialText2.Location = new System.Drawing.Point(19, 46);
+            this.serialText2.Name = "serialText2";
+            this.serialText2.Size = new System.Drawing.Size(100, 29);
+            this.serialText2.TabIndex = 6;
+            this.serialText2.TextChanged += new System.EventHandler(this.serialText2_TextChanged);
+            // 
+            // serialText1
+            // 
+            this.serialText1.Location = new System.Drawing.Point(19, 8);
+            this.serialText1.Name = "serialText1";
+            this.serialText1.Size = new System.Drawing.Size(100, 29);
+            this.serialText1.TabIndex = 5;
+            this.serialText1.TextChanged += new System.EventHandler(this.serialText1_TextChanged);
+            // 
+            // errorText
+            // 
+            this.errorText.AutoSize = true;
+            this.errorText.Font = new System.Drawing.Font("Ubuntu", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorText.ForeColor = System.Drawing.Color.Red;
+            this.errorText.Location = new System.Drawing.Point(165, 123);
+            this.errorText.Name = "errorText";
+            this.errorText.Size = new System.Drawing.Size(41, 16);
+            this.errorText.TabIndex = 4;
+            this.errorText.Text = "label2";
+            this.errorText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.errorText.Click += new System.EventHandler(this.errorText_Click);
+            // 
+            // serial3
+            // 
+            this.serial3.FormattingEnabled = true;
+            this.serial3.Location = new System.Drawing.Point(134, 84);
+            this.serial3.Name = "serial3";
+            this.serial3.Size = new System.Drawing.Size(121, 32);
+            this.serial3.TabIndex = 2;
+            this.serial3.SelectedIndexChanged += new System.EventHandler(this.serial3_SelectedIndexChanged);
+            // 
+            // serial2
+            // 
+            this.serial2.FormattingEnabled = true;
+            this.serial2.Location = new System.Drawing.Point(134, 46);
+            this.serial2.Name = "serial2";
+            this.serial2.Size = new System.Drawing.Size(121, 32);
+            this.serial2.TabIndex = 1;
+            this.serial2.SelectedIndexChanged += new System.EventHandler(this.serial2_SelectedIndexChanged);
+            // 
+            // serial1
+            // 
+            this.serial1.FormattingEnabled = true;
+            this.serial1.Location = new System.Drawing.Point(134, 8);
+            this.serial1.Name = "serial1";
+            this.serial1.Size = new System.Drawing.Size(121, 32);
+            this.serial1.TabIndex = 0;
+            this.serial1.SelectedIndexChanged += new System.EventHandler(this.serial1_SelectedIndexChanged);
+            // 
+            // serialLabel1
+            // 
+            this.serialLabel1.AutoSize = true;
+            this.serialLabel1.Location = new System.Drawing.Point(36, 11);
+            this.serialLabel1.Name = "serialLabel1";
+            this.serialLabel1.Size = new System.Drawing.Size(63, 24);
+            this.serialLabel1.TabIndex = 2;
+            this.serialLabel1.Text = "label1";
+            this.serialLabel1.Click += new System.EventHandler(this.serialLabel1_Click);
+            // 
+            // serialLabel2
+            // 
+            this.serialLabel2.AutoSize = true;
+            this.serialLabel2.Location = new System.Drawing.Point(36, 49);
+            this.serialLabel2.Name = "serialLabel2";
+            this.serialLabel2.Size = new System.Drawing.Size(63, 24);
+            this.serialLabel2.TabIndex = 11;
+            this.serialLabel2.Text = "label2";
+            this.serialLabel2.Click += new System.EventHandler(this.serialLabel2_Click);
+            // 
+            // serialLabel3
+            // 
+            this.serialLabel3.AutoSize = true;
+            this.serialLabel3.Location = new System.Drawing.Point(36, 90);
+            this.serialLabel3.Name = "serialLabel3";
+            this.serialLabel3.Size = new System.Drawing.Size(63, 24);
+            this.serialLabel3.TabIndex = 12;
+            this.serialLabel3.Text = "label3";
+            this.serialLabel3.Visible = false;
+            this.serialLabel3.Click += new System.EventHandler(this.serialLabel3_Click);
             // 
             // Form
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(44)))), ((int)(((byte)(54)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.BottomPanel);
             this.Controls.Add(this.Toolbar);
             this.Font = new System.Drawing.Font("Ubuntu", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(100)))), ((int)(((byte)(117)))));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form";
             this.Text = "RGB Crustacean";
             this.Toolbar.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.BottomPanel.ResumeLayout(false);
+            this.BottomPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -102,6 +263,19 @@
         private System.Windows.Forms.Panel BottomPanel;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox serial3;
+        private System.Windows.Forms.ComboBox serial2;
+        private System.Windows.Forms.ComboBox serial1;
+        private System.Windows.Forms.Label errorText;
+        private System.Windows.Forms.TextBox serialText3;
+        private System.Windows.Forms.TextBox serialText2;
+        private System.Windows.Forms.TextBox serialText1;
+        private System.Windows.Forms.Button serialButton3;
+        private System.Windows.Forms.Button serialButton2;
+        private System.Windows.Forms.Button serialButton1;
+        private System.Windows.Forms.Label serialLabel1;
+        private System.Windows.Forms.Label serialLabel2;
+        private System.Windows.Forms.Label serialLabel3;
     }
 }
 
--- App/Form1.cs
+++ App/Form1.cs
@@ -14,18 +14,72 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
+using System.Net.Http.Headers;
 
 namespace RGB_Crustacean
 {
     public partial class Form : System.Windows.Forms.Form
     {
         String[] ports;
-        SerialPort port;
-        public Gradient[] palettes;
+        Device[] devices = { new Device(), new Device(), new Device() };
 
+        Gradient[] palettes;
+
+        
         public Form()
         {
             InitializeComponent();
+            getAvailableComPorts();
+
+            errorText.Text = "";
+
+            devices[0].button = serialButton1;
+            devices[1].button = serialButton2;
+            devices[2].button = serialButton3;
+
+            devices[0].selection = serial1;
+            devices[1].selection = serial2;
+            devices[2].selection = serial3;
+
+            devices[1].button.Text = "Connect";
+            devices[2].button.Text = "Connect";
+            devices[0].button.Text = "Connect";
+
+            devices[0].nameBox = serialText1;
+            devices[1].nameBox = serialText2;
+            devices[2].nameBox = serialText3;
+
+            serialText1.Text = devices[0].name;
+            serialText2.Text = devices[1].name;
+            serialText3.Text = devices[2].name;
+
+            serialText1.Visible = false;
+            serialText2.Visible = false;
+            serialText3.Visible = false;
+
+            serialLabel1.Visible = true;
+            serialLabel2.Visible = true;
+            serialLabel3.Visible = true;
+
+            serialLabel1.Text = serialText1.Text;
+            serialLabel2.Text = serialText2.Text;
+            serialLabel3.Text = serialText3.Text;
+
+            foreach (string port in ports)
+            {
+                serial1.Items.Add(port);
+                serial2.Items.Add(port);
+                serial3.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    serial1.SelectedItem = ports[0];
+                    serial2.SelectedItem = ports[0];
+                    serial3.SelectedItem = ports[0];
+                }
+            }
+            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -37,33 +91,163 @@ namespace RGB_Crustacean
             ports = SerialPort.GetPortNames();
         }
 
-        private void connectToArduino()
+        private void connectToArduino(int port)
         {
 
             try
             {
+                Device device = devices[port];
+                string selectedPort = device.selection.GetItemText(device.selection.SelectedItem);
                 
-                string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
-                port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
-                port.Open();
-                label4.Text = "";
-                button1.Text = "Disconnect";
-                enableControls();
+                device.isConnected = true;
+                device.port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+                device.button.Text = "Disconnect";
+                errorText.Text = "";
             }
             catch
             {
                 Console.WriteLine("Connection Error");
-                label4.Text = "Connection Error";
+                errorText.Text = "Connection Error";
             }
 
 
         }
+
+        private void disconnectFromArduino(int port)
+        {
+            try
+            {
+                Device device = devices[port];
+                device.isConnected = false;
+                device.port.Write("#STOP\n");
+                Console.WriteLine("#STOP\n");
+                errorText.Text = "";
+                device.button.Text = "Connect";
+            }
+            catch
+            {
+                Console.WriteLine("Connection Error");
+                errorText.Text = "Connection Error"; 
+            }
+        }
+
+        private void serial1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serial2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serial3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialButton1_Click(object sender, EventArgs e)
+        {
+            if (!devices[0].isConnected)
+            {
+                serial1.Enabled = false;
+                connectToArduino(0);
+            }
+            else
+            {
+                serial1.Enabled = true;
+                disconnectFromArduino(0);
+            }
+        }
+
+        private void serialButton2_Click(object sender, EventArgs e)
+        {
+            if (!devices[1].isConnected)
+            {
+                serial2.Enabled = false;
+                connectToArduino(1);
+            }
+            else
+            {
+                serial2.Enabled = true;
+                disconnectFromArduino(1);
+            }
+        }
+
+        private void serialButton3_Click(object sender, EventArgs e)
+        {
+            if (!devices[2].isConnected)
+            {
+                serial3.Enabled = false;
+                connectToArduino(0);
+            }
+            else
+            {
+                serial3.Enabled = true;
+                disconnectFromArduino(0);
+            }
+        }
+        private void serialLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void serialLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialText1_TextChanged(object sender, EventArgs e)
+        {
+            serialLabel1.Text = serialText1.Text;
+            devices[0].name = serialText2.Text;
+            saveDevices(devices[0]);
+        }
+
+        private void serialText2_TextChanged(object sender, EventArgs e)
+        {
+            serialLabel2.Text = serialText2.Text;
+            devices[1].name = serialText2.Text;
+        }
+
+        private void serialText3_TextChanged(object sender, EventArgs e)
+        {
+            serialLabel3.Text = serialText3.Text;
+            devices[2].name = serialText3.Text;
+        }
+
+        private void errorText_Click(object sender, EventArgs e)
+        {
+            errorText.Text = "";
+        }
+        void saveDevices(Device device)
+        {
+            TextWriter txt = new StreamWriter("/device.txt");
+            txt.Write(device.name);
+            txt.Close();
+        }
     }
     public class Gradient
     {
-        public String name;
+        public string name;
         public Color[] color;
         public int[] pos;
+    }
+    public class Device
+    {
+        public string name;
+        public SerialPort port;
+        public bool isConnected = false;
+        public Button button;
+        public TextBox nameBox;
+        public ComboBox selection;
+        
+
     }
     
 }
